7d8f6973d42b7bee23476e25226d425e1257bd2c
charles <wkc1002@naver.com>
2023-03-28 02:33:30 +0900

    first commit

---
+++ src/test/java/hello/core/member/MemoryMemberRepositoryTest.java
@@ -0,0 +1,16 @@
+package hello.core.member;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemoryMemberRepositoryTest {
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void findBuId() {
+    }
+}
