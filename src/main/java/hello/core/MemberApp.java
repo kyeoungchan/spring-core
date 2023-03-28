0db058b396250a0c21c6950f71fb02d95436a66c
charles <wkc1002@naver.com>
2023-03-29 03:07:54 +0900

    spring study commit ch3

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,10 +4,19 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+//        얘가 스프링 컨테이너라고 보면 된다. 얘가 모든 빈 객체들을 다 관리해준다.
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+//        첫 번째 인자로 빈으로 저장된 "메서드" 이름을 넣어준다.
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
