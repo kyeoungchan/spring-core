09a0f978e32c8ecef4cce6679f8f5353b7483d37
charles <wkc1002@naver.com>
2023-04-05 21:00:17 +0900

    spring study auto dependency injection

---
+++ src/main/java/hello/core/HelloLombok.java
@@ -0,0 +1,25 @@
+package hello.core;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class HelloLombok {
+    private String name;
+    private int age;
+
+    public static void main(String[] args) {
+        HelloLombok helloLombok = new HelloLombok();
+        helloLombok.setName("charles");
+
+        String name = helloLombok.getName();
+        System.out.println("name = " + name);
+
+        System.out.println("helloLombok = " + helloLombok);
+        // @ToString 애노테이션을 추가하면 toString() 기능도 해준다.
+    }
+
+}
---
+++ src/main/java/hello/core/annotation/MainDiscountPolicy.java
@@ -0,0 +1,14 @@
+package hello.core.annotation;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import java.lang.annotation.*;
+
+/*Qualifier 애노테이션의 내용들을 복붙한 것들이다.*/
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.TYPE, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Documented
+@Qualifier("mainDiscoutPolicy") // 그러면 MainDiscountPolicy 애노테이션을 쓰면 위 기능이 다 동작한다.
+public @interface MainDiscountPolicy {
+}
